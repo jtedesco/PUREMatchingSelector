c40cd35a68b2477b6d3cfd28abc73e0055a6486f
Jon Tedesco <jon.c.tedesco@gmail.com>
2011-08-07 21:53:53 -0500

    Changed path of input files

--- Run.py
+++ Run.py
@@ -5,7 +5,9 @@ __author__ = 'jtedesco'
 
 
 # Parse the mentees out of it
-parser = CSVParser("/home/jtedesco/sp11PUREapplicants.csv", ',', '"')
+parser = CSVParser("/home/jon/Desktop/sp11PUREapplicants.csv", "/home/jon/Desktop/sp11PUREmentors.csv", ',', '"')
 applicants = parser.parseMentees()
+mentors = parser.parseMentors()
 
 pprint(applicants)
+pprint(mentors)
