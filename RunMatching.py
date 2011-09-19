02dfd2fdf4b423da943344f1503ea25e5d89cf4a
Jon Tedesco <jon@jontedesco.net>
2011-09-18 23:59:52 -0500

    Finished the RunMatching script

--- RunMatching.py
+++ RunMatching.py
@@ -13,6 +13,22 @@ mentees, mentors = parser.parseMentorsAndMentees()
 
 # Perform the matching on the mentors & mentees
 matcher = Matcher(mentors, mentees)
-mentees, mentors = matcher.generateMenteeMentorMatching()
+matching = matcher.generateMenteeMentorMatching()
 
-print "Done"
+# Get the information about the resulting matching
+matchedMentors = matching.getMatchedMentors()
+matchedMentees = matching.getMatchedMentees()
+unmatchedMentors = matching.getUnmatchedMentors()
+unmatchedMentees = matching.getUnmatchedMentees()
+
+# Output the results
+print "Matching:"
+for mentor in matchedMentors:
+    print '\t' + mentor.firstName + ' ' + mentor.lastName
+    for mentee in mentor.mentees:
+        print '\t\t' + mentee.firstName + ' ' + mentee.lastName + '(' + mentee.email + ')'
+print
+
+print "Unmatched Mentees:"
+for mentee in unmatchedMentees:
+    print '\t' + mentee.firstName + ' ' + mentee.lastName + '(' + mentee.email + ')'
