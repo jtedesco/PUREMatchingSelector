32c97f2e3162f133c9ade692d478b5c3c4711c91
Jon Tedesco <jon@jontedesco.net>
2012-09-21 11:35:47 -0500

    Refactored RunPreliminaryMatching script to take user input

--- RunPreliminaryMatching.py
+++ RunPreliminaryMatching.py
@@ -1,15 +1,30 @@
+import os
+import sys
 from src.matcher.Matcher import Matcher
 from src.parser.CSVParser import CSVParser
+from src.constants.UserInputConstants import UserInputConstants
 
 __author__ = 'jtedesco'
 
+# Get input files & output directory
+applicantFileName = raw_input(UserInputConstants.APPLICANT_FILE_PROMPT)
+mentorFileName = raw_input(UserInputConstants.MENTOR_FILE_PROMPT)
+outputDirectoryPath = raw_input(UserInputConstants.OUTPUT_DIRECTORY_PROMPT)
+
+# Check user/input (only existence)
+if not os.path.exists(applicantFileName):
+    print(UserInputConstants.MISSING_FILE_MESSAGE % applicantFileName)
+    sys.exit()
+if not os.path.exists(mentorFileName):
+    print(UserInputConstants.MISSING_FILE_MESSAGE % mentorFileName)
+    sys.exit()
+if not os.path.exists(outputDirectoryPath):
+    os.makedirs(outputDirectoryPath)
 
 # Parse the mentees & mentors out of it
-applicantFile = "/Users/jontedesco/Desktop/mentees.csv"
-mentorFile = "/Users/jontedesco/Desktop/mentors.csv"
-parser = CSVParser(applicantFile, mentorFile, ',', '"')
+parser = CSVParser(applicantFileName, mentorFileName, ',', '"')
 mentees, mentors = parser.parseMentorsAndMentees()
 
 # Generate the output files for each mentor
 matcher = Matcher(mentors, mentees)
-matcher.generateMentorApplicantLists("/Users/jontedesco/Desktop/Mentor Lists/", applicantFile)
+matcher.generateMentorApplicantLists(outputDirectoryPath, applicantFileName)
---
+++ src/constants/UserInputConstants.py
@@ -0,0 +1,9 @@
+__author__ = 'jontedesco'
+
+
+class UserInputConstants(object):
+    APPLICANT_FILE_PROMPT = "Please provide the absolute or relative path to the mentee applicant file (CSV):\n"
+    MENTOR_FILE_PROMPT = "Please provide the absolute or relative path to the mentor file (CSV):\n"
+    OUTPUT_DIRECTORY_PROMPT = "Please provide the absolute or relative path to the output directory for mentor lists:\n"
+
+    MISSING_FILE_MESSAGE = "'%s' path was not found"
