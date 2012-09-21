39f156070d810fbb111052c9d8ff5deed09bfa8e
Jon Tedesco <jon@jontedesco.net>
2012-09-21 11:37:21 -0500

    Added user input to RunMatching script

--- RunMatching.py
+++ RunMatching.py
@@ -1,13 +1,26 @@
+import os
+import sys
+from src.constants.UserInputConstants import UserInputConstants
 from src.matcher.Matcher import Matcher
 from src.parser.CSVParser import CSVParser
 
 __author__ = 'Jon Tedesco'
 
+# Get input files & output directory
+applicantFileName = raw_input(UserInputConstants.APPLICANT_FILE_PROMPT)
+mentorFileName = raw_input(UserInputConstants.MENTOR_FILE_PROMPT)
+outputDirectoryPath = raw_input(UserInputConstants.OUTPUT_DIRECTORY_PROMPT)
+
+# Check user/input (only existence)
+if not os.path.exists(applicantFileName):
+    print(UserInputConstants.MISSING_FILE_MESSAGE % applicantFileName)
+    sys.exit()
+if not os.path.exists(mentorFileName):
+    print(UserInputConstants.MISSING_FILE_MESSAGE % mentorFileName)
+    sys.exit()
 
 # Parse the mentees & mentors out of it
-applicantFile = "/Users/jontedesco/Desktop/mentees.csv"
-mentorFile = "/Users/jontedesco/Desktop/mentors.csv"
-parser = CSVParser(applicantFile, mentorFile, ',', '"')
+parser = CSVParser(applicantFileName, mentorFileName, ',', '"')
 mentees, mentors = parser.parseMentorsAndMentees()
 
 # Perform the matching on the mentors & mentees
